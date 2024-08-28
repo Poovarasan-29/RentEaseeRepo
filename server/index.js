19b1412487c7c1f65cf6e5a697d12087b5dd57cd
Poovarasan-29 <spking222005@gmail.com>
2024-08-29 04:48:38 +0530

    chnage the default root

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', newUserRegistration);
+app.get('/', getDrivers);
 
 app.get('/rent-cars', getCars)
 app.get('/car-details/:id', getSingleCarFullDetails)
