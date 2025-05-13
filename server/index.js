aea07c6f4023e0de75bf3f8331898d55ee07079a
Poovarasan-29 <spking222005@gmail.com>
2025-05-13 19:51:58 +0530

    Production : start router added in server

--- server/index.js
+++ server/index.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const path = require('path');
-const cors = require('cors');
-const connectDatabase = require('./config/connectDB');
-const carRouter = require('./routes/car');
-const driverRouter = require('./routes/driver');
-const authenticationRouter = require('./routes/authentication');
-const userProfileRouter = require('./routes/userProfile');
+const dotenv = require("dotenv");
+const path = require("path");
+const cors = require("cors");
+const connectDatabase = require("./config/connectDB");
+const carRouter = require("./routes/car");
+const driverRouter = require("./routes/driver");
+const authenticationRouter = require("./routes/authentication");
+const userProfileRouter = require("./routes/userProfile");
 
-dotenv.config({ path: path.join(__dirname, '.env') })
+dotenv.config({ path: path.join(__dirname, ".env") });
 const port = process.env.PORT || 5000;
 
 connectDatabase();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 app.use(carRouter);
 app.use(driverRouter);
 app.use(authenticationRouter);
 app.use(userProfileRouter);
 
+app.get("/", (req, res) => {
+  console.log("Server Running ");
+});
+
 app.listen(port, () => {
-    console.log("Running at Port : ", port);
+  console.log("Running at Port : ", port);
 });
